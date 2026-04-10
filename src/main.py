288c56e47d9ba0ebc9478b9e251b9ca4712f9ab3
Andreas Schlager <andreas.schlager@stud.plus.ac.at>
2026-04-10 16:25:29 +0200

    Read args and run jxl for all images with fixed quality

--- src/main.py
+++ src/main.py
@@ -1,1 +1,23 @@
-print('Hello world!')
+import typer
+from tqdm import tqdm
+from pathlib import Path
+import subprocess
+
+out_dir = Path('/images/out')
+tmp_dir = Path('/images/out/tmp')
+
+def run_jxl_tool(quality: int, path: Path):
+    out_file = out_dir / (path.stem + '.jxl')
+    subprocess.run(['cjxl', str(path), str(out_file), '-q', str(quality)], 
+                   stdout=subprocess.DEVNULL,
+                   stderr=subprocess.DEVNULL)
+
+def main(file_paths: list[str]):
+    out_dir.mkdir(parents=True, exist_ok=True)
+
+    for path in tqdm([Path(x) for x in file_paths]):
+        if path.exists():
+            run_jxl_tool(20, path)
+
+if __name__ == "__main__":
+    typer.run(main)
