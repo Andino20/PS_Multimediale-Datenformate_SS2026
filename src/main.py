21b7ffcc46001285498abd2114f5d9305d9c52f8
Andreas Schlager <andreas.schlager@stud.plus.ac.at>
2026-06-30 11:08:26 +0200

    Update main script

--- src/main.py
+++ src/main.py
@@ -5,12 +5,13 @@ from collections.abc import Callable
 import subprocess
 
 out_dir = Path('imgs')
+target_size_kb = 0
 
-def get_out_file_path(img: Path, extension: str) -> Path:
-    return out_dir / img.with_suffix(extension).name
+def get_out_file_path(img: Path, variant: str) -> Path:
+    return out_dir / f'{variant}_{int(target_size_kb)}_{img.stem}.{variant}'
 
 def jpeg_tool(img: Path, quality: int) -> int:
-    out_file = get_out_file_path(img=img, extension='.jpeg')
+    out_file = get_out_file_path(img=img, variant='jpeg')
     with open(out_file, "wb") as f:
         subprocess.run(['cjpeg', '-quality', str(quality), str(img)], 
                     stdout=f,
@@ -18,22 +19,22 @@ def jpeg_tool(img: Path, quality: int) -> int:
     return out_file.stat().st_size
 
 def j2k_tool(img: Path, quality: int) -> int:
-    out_file = get_out_file_path(img=img, extension='.j2k')
+    out_file = get_out_file_path(img=img, variant='j2k')
     subprocess.run(['opj_compress', '-r', str(101 - quality), '-i', str(img), '-o', str(out_file)], 
                 stdout=subprocess.DEVNULL,
                 stderr=subprocess.DEVNULL)
     return out_file.stat().st_size
 
 def jxl_tool(img: Path, quality: int) -> int:
-    out_file = get_out_file_path(img=img, extension='.jxl')
+    out_file = get_out_file_path(img=img, variant='jxl')
     subprocess.run(['cjxl', str(img), str(out_file), '-q', str(quality)], 
                 stdout=subprocess.DEVNULL,
                 stderr=subprocess.DEVNULL)
     return out_file.stat().st_size
 
 def jxr_tool(img: Path, quality: int) -> int:
-    out_file = get_out_file_path(img=img, extension='.jxr')
-    subprocess.run(['JxrEncApp', '-q', str(quality), '-i', str(img), '-o', str(out_file), '-c', '10'], 
+    out_file = get_out_file_path(img=img, variant='jxr')
+    subprocess.run(['JxrEncApp', '-i', str(img), '-o', str(out_file), '-q', str(quality), '-c', '0'], 
                 stdout=subprocess.DEVNULL,
                 stderr=subprocess.DEVNULL)
     return out_file.stat().st_size
@@ -68,7 +69,8 @@ def main(tool: str, target_size: int, file_paths: list[Path]):
     assert tool in available_tools, 'Tool has to be one of jpeg, j2k, jxl or jxr'
     
     global out_dir
-    out_dir = Path(*['imgs', 'out', tool, str(target_size)])
+    global target_size_kb
+    target_size_kb = target_size / 1000
     out_dir.mkdir(parents=True, exist_ok=True)
 
     for img in tqdm(file_paths, f'{tool} conversion to {target_size}B'):
